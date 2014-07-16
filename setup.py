32dbcea7df2d720fbedc78d2e7ceeb329e217e7c
James Tauber <jtauber@jtauber.com>
2014-07-15 20:45:27 -0400

    initial setup file for midiwrite

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="midiwrite",
+    version="0.1",
+    description="lightweight module for writing MIDI files",
+    license="MIT",
+    url="http://github.com/jtauber/midiwrite",
+    author="James Tauber",
+    author_email="jtauber@jtauber.com",
+    py_modules=["midiwrite"],
+)
